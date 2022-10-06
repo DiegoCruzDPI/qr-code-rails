3e2418129aab0008b2ea70160aa40e22dea97058
DiegoCruz02 <a.diegocruz02@gmail.com>
2022-10-06 17:33:18 +0000

    QR Code ruby rails app I made an app that generates a QR code for the user. So far only the text generates a QR code. More work needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   def homepage
     render({ :template => "my_templates/homepage.html.erb" })
   end
+  #text
+  def text
+      #Parameters: {"content"=>"hi there"} This is the querey that we can use on the stie to 
+      @user_text = params.fetch("content")
+      @qrcode = RQRCode::QRCode.new(@user_text)
+
+    render({ :template => "my_templates/text.html.erb"})
+
+  end
+
+  #url
+  def url
+    render({ :template => "my_templates/url.html.erb"})
+  end
+  
+  #wifi
+  def wifi
+    render({ :template => "my_templates/wifi.html.erb"})
+  end
+  
+  #sms
+  def sms
+    render({ :template => "my_templates/sms.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   # Write your routes here. URLs to support:
   
   # /text
+  get("/text", {:controller => "application", :action => "text"})
+
   # /url
+  get("/url", {:controller => "application", :action => "url"})
   # /wifi
+  get("/wifi", {:controller => "application", :action => "wifi"})
   # /sms
-  
+  get("/sms", {:controller => "application", :action => "sms"})
+  #/  
   get("/", {:controller => "application", :action => "homepage"})
 
   # Solutions below.
